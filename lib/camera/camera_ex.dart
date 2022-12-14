0bc508f4c210cce72183522640284ccdd92329c4
HuyHuynh <huy.huynh>
2022-12-14 16:09:27 +0700

    added pick image from local

--- lib/camera/camera_ex.dart
+++ lib/camera/camera_ex.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class PickImage extends StatefulWidget {
   const PickImage({Key? key}) : super(key: key);
@@ -28,40 +29,79 @@ class _PickImageState extends State<PickImage> {
     _onPressed() async {
       Navigator.of(context).push(
         MaterialPageRoute(
-          builder: (context) => TakePictureScreen(onCroppedFile: (croppedFile) {
-            if (croppedFile != null) {
-              setState(() {
-                _croppedFile = croppedFile;
-              });
+          builder: (context) => TakePictureScreen(onPickedFile: (pickedFile) {
+            if (pickedFile != null) {
+              _cropImage(
+                pickedFile: pickedFile,
+                onCroppedFile: (croppedFile) {
+                  setState(() {
+                    _croppedFile = croppedFile;
+                  });
+                },
+              );
             }
           }),
         ),
       );
     }
 
-    if (_croppedFile != null) {
-      return Center(
-        child: InkWell(
-          onTap: _onPressed,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Card(
-              elevation: 4.0,
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: _image(),
+    _buildImage() {
+      if (_croppedFile != null) {
+        return Center(
+          child: InkWell(
+            onTap: _onPressed,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Card(
+                elevation: 4.0,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: _image(),
+                ),
               ),
             ),
           ),
-        ),
-      );
+        );
+      }
+      return Text('no image');
     }
+
     return Center(
-      child: ElevatedButton.icon(
-        //image capture button
-        onPressed: _onPressed,
-        icon: Icon(Icons.camera),
-        label: Text("Capture"),
+      child: Column(
+        children: [
+          _buildImage(),
+          SizedBox(
+            width: 150,
+            child: ElevatedButton.icon(
+              //image capture button
+              onPressed: _onPressed,
+              icon: Icon(Icons.camera),
+              label: Text("Capture"),
+            ),
+          ),
+          SizedBox(
+            width: 150,
+            child: ElevatedButton.icon(
+              onPressed: () async {
+                final pickedFile = await ImagePicker().pickImage(
+                  source: ImageSource.gallery,
+                );
+                if (pickedFile != null) {
+                  _cropImage(
+                    pickedFile: pickedFile,
+                    onCroppedFile: (croppedFile) {
+                      setState(() {
+                        _croppedFile = croppedFile;
+                      });
+                    },
+                  );
+                }
+              },
+              icon: Icon(Icons.photo),
+              label: Text("Photo"),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -82,11 +122,11 @@ class _PickImageState extends State<PickImage> {
 
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
-  final Function(CroppedFile?)? onCroppedFile;
+  final Function(XFile?)? onPickedFile;
 
   const TakePictureScreen({
     Key? key,
-    required this.onCroppedFile,
+    required this.onPickedFile,
   }) : super(key: key);
 
   @override
@@ -96,7 +136,6 @@ class TakePictureScreen extends StatefulWidget {
 class TakePictureScreenState extends State<TakePictureScreen> {
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
-  XFile? xFile;
 
   loadCamera() async {
     final cameras = await availableCameras();
@@ -137,198 +176,72 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   }
 
   Widget _body() {
-    if (xFile == null) {
-      return Stack(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: CameraPreview(_controller),
-          ),
-          Positioned(
-            bottom: 20.0,
-            right: 0.0,
-            left: 0.0,
-            child: FloatingActionButton(
-              heroTag: null,
-              onPressed: () async {
-                try {
-                  await _initializeControllerFuture;
-                  final image = await _controller.takePicture();
-                  if (!mounted) return;
-
-                  setState(() {
-                    xFile = image;
-                  });
-                } catch (e) {
-                  print(e);
-                }
-              },
-              backgroundColor: Colors.redAccent,
-              tooltip: 'camera',
-              child: const Icon(Icons.camera),
-            ),
-          ),
-          Positioned(
-            left: 20.0,
-            top: 35.0,
-            child: InkWell(
-              onTap: () => Navigator.pop(context),
-              child: Icon(
-                Icons.clear,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      );
-    }
-    return _DisplayPictureScreen(
-      pickedFile: xFile,
-      onCroppedFile: widget.onCroppedFile,
-    );
-  }
-}
-
-class _DisplayPictureScreen extends StatefulWidget {
-  final XFile? pickedFile;
-  final Function(CroppedFile?)? onCroppedFile;
-
-  const _DisplayPictureScreen({
-    Key? key,
-    required this.pickedFile,
-    this.onCroppedFile,
-  }) : super(key: key);
-
-  @override
-  State<_DisplayPictureScreen> createState() => _DisplayPictureScreenState();
-}
-
-class _DisplayPictureScreenState extends State<_DisplayPictureScreen> {
-  XFile? _pickedFile;
-  CroppedFile? _croppedFile;
-
-  @override
-  void initState() {
-    _pickedFile = widget.pickedFile;
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(title: Text('title')),
-      body: _body(),
-    );
-  }
-
-  Widget _body() {
-    if (_croppedFile != null || _pickedFile != null) {
-      return _imageCard();
-    } else {
-      return TakePictureScreen(
-        onCroppedFile: widget.onCroppedFile,
-      );
-    }
-  }
-
-  Widget _imageCard() {
-    return Container(
-      color: Colors.black,
-      width: double.infinity,
-      height: double.infinity,
-      child: Stack(
-        children: [
-          _buildImage(),
-          Positioned(
-            bottom: 20.0,
-            right: 0.0,
-            left: 0.0,
-            child: _menu(),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _imageDisplay(String path) {
-    return Image.file(
-      File(path),
-      fit: BoxFit.cover,
-      height: double.infinity,
-      width: double.infinity,
-    );
-  }
-
-  Widget _buildImage() {
-    if (_croppedFile != null) {
-      return _imageDisplay(_croppedFile!.path);
-    } else if (_pickedFile != null) {
-      return _imageDisplay(_pickedFile!.path);
-    } else {
-      return const SizedBox.shrink();
-    }
-  }
-
-  Widget _menu() {
-    return Center(
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          FloatingActionButton(
+    return Stack(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: CameraPreview(_controller),
+        ),
+        Positioned(
+          bottom: 20.0,
+          right: 0.0,
+          left: 0.0,
+          child: FloatingActionButton(
             heroTag: null,
-            onPressed: () {
-              _clear();
+            onPressed: () async {
+              try {
+                await _initializeControllerFuture;
+                final image = await _controller.takePicture();
+                if (!mounted) return;
+                widget.onPickedFile?.call(image);
+                Navigator.pop(context);
+              } catch (e) {
+                print(e);
+              }
             },
             backgroundColor: Colors.redAccent,
-            tooltip: 'Delete',
-            child: const Icon(Icons.delete),
+            tooltip: 'camera',
+            child: const Icon(Icons.camera),
           ),
-          if (_croppedFile == null)
-            Padding(
-              padding: const EdgeInsets.only(left: 32.0),
-              child: FloatingActionButton(
-                heroTag: null,
-                onPressed: () {
-                  _cropImage();
-                },
-                backgroundColor: const Color(0xFFBC764A),
-                tooltip: 'Crop',
-                child: const Icon(Icons.crop),
-              ),
+        ),
+        Positioned(
+          left: 20.0,
+          top: 35.0,
+          child: InkWell(
+            onTap: () => Navigator.pop(context),
+            child: Icon(
+              Icons.clear,
+              color: Colors.white,
             ),
-        ],
-      ),
+          ),
+        ),
+      ],
     );
   }
+}
 
-  Future<void> _cropImage() async {
-    if (_pickedFile != null) {
-      final croppedFile = await ImageCropper().cropImage(
-        sourcePath: _pickedFile!.path,
-        compressFormat: ImageCompressFormat.jpg,
-        compressQuality: 100,
-        uiSettings: [
-          AndroidUiSettings(
-              toolbarTitle: 'Cropper',
-              toolbarColor: Colors.black,
-              toolbarWidgetColor: Colors.white,
-              initAspectRatio: CropAspectRatioPreset.original,
-              lockAspectRatio: false),
-          IOSUiSettings(
-            title: 'Cropper',
-          ),
-        ],
-      );
-      Navigator.pop(context);
-      widget.onCroppedFile?.call(croppedFile);
-    }
-  }
-
-  void _clear() {
-    setState(() {
-      _pickedFile = null;
-      _croppedFile = null;
-    });
+Future<void> _cropImage({
+  XFile? pickedFile,
+  Function(CroppedFile?)? onCroppedFile,
+}) async {
+  if (pickedFile != null) {
+    final croppedFile = await ImageCropper().cropImage(
+      sourcePath: pickedFile.path,
+      compressFormat: ImageCompressFormat.jpg,
+      compressQuality: 100,
+      uiSettings: [
+        AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.black,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        IOSUiSettings(
+          title: 'Cropper',
+        ),
+      ],
+    );
+    onCroppedFile?.call(croppedFile);
   }
 }
